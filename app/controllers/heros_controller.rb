ccf16875ed1e76cd72ec0d527e22782ee5e04294
Segjr <samuel8016@gmail.com>
2022-08-04 16:41:34 -0700

    endpoints added

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,30 @@
 class HerosController < ApplicationController
+
+    def index
+        heros = Hero.all;
+        render json: heros
+    end
+
+    def create
+        hero = Hero.create(hero_params)
+        render json: hero 
+    end
+
+    def update
+        hero = Hero.find(params[:id])
+        hero.update(hero_params)
+        render json: hero    
+    end
+
+    def destroy
+        hero = Hero.find(params[:id])
+        hero.destroy
+        render json: hero
+    end
+
+    private
+    def hero_params
+        params.require(:hero).permit(:name, :age, :enjoys, :image)
+    end
+
 end
--- spec/requests/heros_spec.rb
+++ spec/requests/heros_spec.rb
@@ -1,7 +1,93 @@
 require 'rails_helper'
 
-RSpec.describe "Heros", type: :request do
+RSpec.describe "Hero", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of heros" do
+      Hero.create(
+        name: "Gamora",
+        age: 29,
+        enjoys: "Listening to 70's jams while cruising through the galaxy",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQay-ueFvf3PjCBaFyJJUTkDNFHXwo6lFRVmA&usqp=CAU"
+      )
+
+      get '/heros/'
+      hero = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(hero.length).to eq 1
+    end
   end
+
+
+  describe "POST /create" do
+    it "creates a hero" do
+      hero_params = {
+        hero: {
+          name: "Gamora",
+          age: 29,
+          enjoys: "Listening to 70's jams while cruising through the galaxy",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQay-ueFvf3PjCBaFyJJUTkDNFHXwo6lFRVmA&usqp=CAU"
+        }
+      }
+
+      post '/heros', params: hero_params
+      expect(response).to have_http_status(200)
+      hero = Hero.first
+      expect(hero.name).to eq "Gamora"
+      expect(hero.age).to eq 29
+      expect(hero.enjoys).to eq "Listening to 70's jams while cruising through the galaxy"
+      expect(hero.image).to eq "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQay-ueFvf3PjCBaFyJJUTkDNFHXwo6lFRVmA&usqp=CAU"
+    end
+  end
+
+
+  describe "PATCH /update" do
+    it "updates a hero" do
+      hero_params = {
+        hero: {
+          name: "Gamora",
+          age: 29,
+          enjoys: "Listening to 70's jams while cruising through the galaxy",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQay-ueFvf3PjCBaFyJJUTkDNFHXwo6lFRVmA&usqp=CAU"
+        }
+      }
+
+      post '/heros', params: hero_params
+      hero = Hero.first
+      updated_hero_params = {
+        hero: {
+          name: "Gamora",
+        age: 30,
+        enjoys: "Listening to 70's jams while cruising through the galaxy",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQay-ueFvf3PjCBaFyJJUTkDNFHXwo6lFRVmA&usqp=CAU"
+        }
+      }
+
+      patch "/heros/#{hero.id}", params: updated_hero_params
+      updated_hero = Hero.find(hero.id)
+      expect(response).to have_http_status(200)
+      expect(updated_hero.age).to eq 30
+    end
+  end
+
+
+  describe "DELETE /destroy" do
+    it "deletes a hero" do
+      hero_params = {
+        hero: {
+          name: "Gamora",
+          age: 29,
+          enjoys: "Listening to 70's jams while cruising through the galaxy",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQay-ueFvf3PjCBaFyJJUTkDNFHXwo6lFRVmA&usqp=CAU"
+        }
+      }
+
+      post '/heros', params: hero_params
+      hero = Hero.first
+      delete "/heros/#{hero.id}"
+      expect(response).to have_http_status(200)
+      heros = Hero.all;
+      expect(heros).to be_empty
+    end
+  end 
+
 end
